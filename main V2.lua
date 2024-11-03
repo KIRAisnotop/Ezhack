1b5f3e2159fbc70061e19851293300ef2e6439b9
KIRAisnotop <kirapvpytch@gmail.com>
2024-11-02 19:17:29 -0500

    Update main V2.lua

--- main V2.lua
+++ main V2.lua
@@ -28,6 +28,62 @@ local Window = Rayfield:CreateWindow({
 
 local MainTab = Window:CreateTab("Main", 4483362458)
 
+-- Kill self button
+MainTab:CreateButton({
+    Name = "Kill Self",
+    Callback = function()
+        local Players = game:GetService("Players")
+        local player = Players.LocalPlayer
+        if player.Character and player.Character:FindFirstChild("Humanoid") then
+            player.Character.Humanoid.Health = 0
+        end
+    end,
+})
+
+-- Testing button
+MainTab:CreateButton({
+    Name = "Testing (press F9)",
+    Callback = function()
+        print("testing")
+        local player = game.Players.LocalPlayer
+        if player.Character and player.Character:FindFirstChild("Humanoid") then
+            player.Character.Humanoid.Health = 0
+        end
+    end,
+})
+
+-- Speed slider
+MainTab:CreateSlider({
+    Name = "Speed Hacks",
+    Range = {16, 300},
+    Increment = 1,
+    Suffix = "Stud",
+    CurrentValue = 16,
+    Flag = "Slider1",
+    Callback = function(Value)
+        local player = game.Players.LocalPlayer
+        local character = player.Character or player.CharacterAdded:Wait()
+        local humanoid = character:WaitForChild("Humanoid")
+        humanoid.WalkSpeed = Value
+    end,
+})
+
+-- Jump Power slider
+MainTab:CreateSlider({
+    Name = "Jump Power",
+    Range = {50, 300},
+    Increment = 1,
+    Suffix = "Stud",
+    CurrentValue = 50,
+    Flag = "Slider2",
+    Callback = function(Value)
+        local player = game.Players.LocalPlayer
+        local character = player.Character or player.CharacterAdded:Wait()
+        local humanoid = character:WaitForChild("Humanoid")
+        humanoid.JumpPower = Value
+    end,
+})
+
 -- ESP Tab
 local ESPTab = Window:CreateTab("ESP", 4483362458)
 
